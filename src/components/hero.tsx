f9a24cbb2294ea2d65a59a3e02e5ec1d9e4aad58
ionodeionode <144650818+ionodeionode@users.noreply.github.com>
2025-08-03 13:17:48 +0700

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -28,7 +28,7 @@ export function Hero() {
           transition={{ duration: 0.5, delay: 0.1 }}
           className="text-5xl md:text-7xl font-bold tracking-tight"
         >
-          IONode.Top
+          IONODE
         </motion.h1>
         <motion.h2
           initial={{ opacity: 0, y: 20 }}
@@ -54,7 +54,7 @@ export function Hero() {
           transition={{ duration: 0.5, delay: 0.4 }}
           className="text-xl font-semibold italic"
         >
-          &quot;JOIN WITH IONODE.TOP !&quot;
+          &quot;JOIN WITH IONODE !&quot;
         </motion.p>
         <motion.div
           initial={{ opacity: 0, y: 20 }}
